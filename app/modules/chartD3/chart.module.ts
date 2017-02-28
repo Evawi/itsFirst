02d2742d4aad327923f2ed4fa4221f87854b902c
a.konopleva <konopleva@crazypanda.ru>
2017-02-28 16:15:28 +0300

    chart

--- app/modules/chartD3/chart.module.ts
+++ app/modules/chartD3/chart.module.ts
@@ -1,13 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-
+import { HttpModule }    from '@angular/http';
 import { ChartComponent }         from './chart.component';
 
 //DESCR: инит d3
 
 @NgModule({
-    imports:      [ BrowserModule, FormsModule ],
+    imports:      [ BrowserModule,HttpModule, FormsModule ],
     declarations: [ ChartComponent ],
     bootstrap:    [ ChartComponent ],
     exports:      [ ChartComponent ]
